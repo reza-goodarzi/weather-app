147da3b1e61c7e8faf50b079b3c96997c6d89f8c
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-07-30 20:27:52 +0430

    refcator apis endpoints

--- src/lib/api.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = "http://api.weatherapi.com/v1";
-
-export const endpoints = {
-  current: `${API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&aqi=no&q=`,
-  search: `${API_URL}/search.json?key=${process.env.REACT_APP_API_KEY}&q=`,
-};
--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
-import { endpoints } from "../lib/api";
+import { API_URL } from "../lib/constant";
 
-export const getCurrentWether = async (local = "london") => {
+export const getCurrentWether = async (local = "london", aqi = "no") => {
   try {
-    const response = await fetch(`${endpoints.current}${local}`);
+    const response = await fetch(
+      `${API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&aqi=${aqi}&q=${local}`
+    );
 
     const data = await response.json();
 
@@ -18,7 +20,9 @@ export const getCurrentWether = async (local = "london") => {
 
 export const searchCity = async (local) => {
   try {
-    const response = await fetch(`${endpoints.search}${local}`);
+    const response = await fetch(
+      `${API_URL}/search.json?key=${process.env.REACT_APP_API_KEY}&q=${local}`
+    );
 
     const data = await response.json();
 
